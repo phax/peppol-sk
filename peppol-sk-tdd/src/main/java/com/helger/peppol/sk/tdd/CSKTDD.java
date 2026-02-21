a80275d2fff5d7b5917804f86c7c1bee2b8ea856
Philip Helger <philip@helger.com>
2026-02-21 21:19:46 +0100

    Updated note

--- peppol-sk-tdd/src/main/java/com/helger/peppol/sk/tdd/CSKTDD.java
+++ peppol-sk-tdd/src/main/java/com/helger/peppol/sk/tdd/CSKTDD.java
@@ -16,7 +16,7 @@ public final class CSKTDD
   /**
    * The following namespace is a type-4 UUID from the Peppol ViDA Pilot solution architecture
    */
-  // TODO get correct UUID
+  // TODO get correct UUID (same as ViDA pilot)
   public static final UUID PEPPOL_SK_NAMESPACE = UUID.fromString ("e0bc4ac8-b025-46e5-a76d-0c893fc3027e");
 
   private CSKTDD ()
